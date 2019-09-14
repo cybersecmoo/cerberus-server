3d229054db16a89958f9bccfc3b9cdbf16534484
cybersecmoo <50770700+cybersecmoo@users.noreply.github.com>
2019-09-14 23:53:42 +0100

    Generic List Didn't Really Work Out

--- client/src/components/genericlist/GenericList.component.jsx
+++
@@ -1,3 +0,0 @@
-// A simple, generic list component, which can take an array of other components to display in a stack format
-// Not sure if this will actually work the way I want it to, though. If so, will have to create a different list component
-// for each thing that will be listed (users, queued commands, command types, targets...)
