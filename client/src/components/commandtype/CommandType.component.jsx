3754507b0026d869afbd0b6597c762fcbf373f83
cybersecmoo <50770700+cybersecmoo@users.noreply.github.com>
2019-10-18 23:32:44 +0100

    Begin Setting up Components for Command List

--- client/src/components/commandtype/CommandType.component.jsx
+++ client/src/components/commandtype/CommandType.component.jsx
@@ -1,15 +1,52 @@
-// Displays a command type: name, and number of arguments, and a rubbish-bin icon for deletion
+// Contains username, whether they are an admin, and a rubbish-bin icon, to delete them
 import React, { Component } from "react";
+import "./CommandType.style.scss";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import { setAlert } from "../../../../redux/alert/alert.action";
+import { deleteCommandType } from "../../../../redux/users/users.action";
 
 class CommandType extends Component {
+  deleteClicked = async event => {
+    event.preventDefault();
+
+    try {
+      this.props.deleteCommandType(this.props.commandType._id);
+    } catch (err) {
+      this.props.setAlert(err, "danger");
+    }
+  };
+
   render() {
-    <div className="commandtype-summary">
-      <h3>Name: </h3>
-      {this.props.commandType.name}
-      <h3>Args: </h3>
-      {this.props.commandType.argsCount}
-    </div>;
+    return (
+      <ListGroup.Item eventKey={this.props.key} variant="dark">
+        <span className="cell">
+          <b>Name: </b>
+          {this.props.commandType.name}
+        </span>
+        <span className="cell">
+          <b>Args: </b>
+          {this.props.commandType.numArgs}
+        </span>
+        <Button variant="danger" onClick={this.deleteClicked}>
+          Delete
+        </Button>
+      </ListGroup.Item>
+    );
   }
 }
 
-export default CommandType;
+CommandType.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  deleteCommandType: PropTypes.func.isRequired,
+  commandType: PropTypes.object.isRequired
+};
+
+export default connect(
+  null,
+  { setAlert, deleteCommandType }
+)(CommandType);
---
+++ client/src/components/commandtype/CommandTypeList.component.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import CommandType from "./CommandType.component";
+import "./UserList.style.scss";
+import ListGroup from "react-bootstrap/ListGroup";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { fetchCommands } from "../../../../redux/users/users.action";
+import { setAlert } from "../../../../redux/alert/alert.action";
+import NewCommandType from "./NewCommandType.component";
+
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+class CommandTypeList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+    this.handleShow = this.handleShow.bind(this);
+    this.handleHide = this.handleHide.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.props.fetchUsers();
+  }
+
+  handleShow() {
+    this.setState({
+      showModal: true
+    });
+  }
+
+  handleHide() {
+    this.setState({
+      showModal: false
+    });
+  }
+
+  render() {
+    const commandTypes = this.props.allCommands;
+
+    if (commandTypes && commandTypes !== null && commandTypes.length > 0) {
+      return (
+        <div className="commandTypes">
+          <h3>Command Types</h3>
+          <ListGroup className="commandTypes-list">
+            {this.props.allCommands.map(item => (
+              <CommandType key={item._id} commandType={item}></CommandType>
+            ))}
+          </ListGroup>
+          <Button className="newCommandType" variant="success" onClick={this.handleShow}>
+            New
+          </Button>
+
+          <Modal size="lg" show={this.state.showModal} onHide={this.handleHide} centered>
+            <NewCommandType />
+          </Modal>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+}
+
+CommandTypeList.propTypes = {
+  allCommands: PropTypes.array.isRequired,
+  fetchCommands: PropTypes.func.isRequired,
+  setAlert: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  allCommands: state.commands.allCommands
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchCommands, setAlert }
+)(CommandTypeList);
